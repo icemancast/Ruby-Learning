a9094424abb065ebe4cd8336db2baf70c955d667
Isaac Castillo <icemancast@gmail.com>
2015-10-20 10:58:41 -0500

    changes to convert file

--- 1_week/1wk_6e_convert_to_celcius.rb
+++ 1_week/1wk_6e_convert_to_celcius.rb
@@ -1,6 +1,6 @@
-def convert_to_celcius tempature
-  temp = (tempature.to_f - 32) * 5 / 9
-  format("%.2f", temp)
+def convert tempature
+  (tempature.to_f - 32) * 5 / 9
 end
 
-puts convert_to_celcius 100
+puts convert 100
+puts convert 100 + 3
